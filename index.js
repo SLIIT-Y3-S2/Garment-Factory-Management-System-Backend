ae35be689882729fcb4715305a84e01ca09042f9
Dishancp <it20012106@my.sliit.lk>
2022-08-26 10:00:07 +0530

    Connect With Database

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./src/config/config");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.json("Hello Node!");
+});
+
+// const vehicleApi = require("./src/api/vehicle.api");
+// app.use("/vehicle", vehicleApi());
+
+
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
+
---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+const DB =  "mongodb+srv://DRCC:SER001@garment-factory-managem.qg73eoz.mongodb.net/Garment_Factory_Management_System?retryWrites=true&w=majority";
+
+const connectDB = async () => {
+  await mongoose.connect(DB);
+  console.log("Database Connected");
+};
+
+module.exports = connectDB;
